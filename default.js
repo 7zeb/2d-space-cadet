9c78d6c656a6a310024691f8bf3a7494755ff52e
7Zeb <66693249+7zeb@users.noreply.github.com>
2025-07-28 17:24:00 +0530

    added the frame

---
+++ default.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(600, 800);
+  ball = createVector(width / 2, height / 2);
+  velocity = createVector(2, -3);
+}
+
+function draw() {
+    background(20);
+    fill(156, 151, 140); //set ball colour to a gray-ish
+    ellipse(ball.x, ball.y, 20); //draws the ball
+    //ball.x and ball.y are both variables
+    
+    //update the balls position
+    ball.add(velocity);
+
+    // Bounce off walls
+     if (ball.x <= 0 || ball.x >= width) velocity.x *= -1;
+     if (ball.y <= 0 || ball.y >= height) velocity.y *= -1;
+}
